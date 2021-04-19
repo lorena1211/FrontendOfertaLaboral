913f56b470088fae220655a82390092b74e1a05e
Lorena Naranjo <lorena.1701814799@ucaldas.edu.co>
2021-04-18 21:53:55 -0500

    Inicio Frontend Oferta Laboral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListarDepartamentoComponent } from './modulos/parametrizacion/departamento/listar-departamento/listar-departamento.component';
-import { CrearDepartamentoComponent } from './modulos/parametrizacion/departamento/crear-departamento/crear-departamento.component';
-import { EditarDepartamentoComponent } from './modulos/parametrizacion/departamento/editar-departamento/editar-departamento.component';
-import { EliminarDepartamentoComponent } from './modulos/parametrizacion/departamento/eliminar-departamento/eliminar-departamento.component';
-import { ListarCiudadComponent } from './modulos/parametrizacion/ciudad/listar-ciudad/listar-ciudad.component';
-import { CrearCiudadComponent } from './modulos/parametrizacion/ciudad/crear-ciudad/crear-ciudad.component';
-import { EditarCiudadComponent } from './modulos/parametrizacion/ciudad/editar-ciudad/editar-ciudad.component';
-import { EliminarCiudadComponent } from './modulos/parametrizacion/ciudad/eliminar-ciudad/eliminar-ciudad.component';
-import { ListarHabilidadComponent } from './modulos/parametrizacion/habilidad/listar-habilidad/listar-habilidad.component';
-import { CrearHabilidadComponent } from './modulos/parametrizacion/habilidad/crear-habilidad/crear-habilidad.component';
-import { EditarHabilidadComponent } from './modulos/parametrizacion/habilidad/editar-habilidad/editar-habilidad.component';
-import { EliminarHabilidadComponent } from './modulos/parametrizacion/habilidad/eliminar-habilidad/eliminar-habilidad.component';
-import { ListarProfesionComponent } from './modulos/parametrizacion/profesion/listar-profesion/listar-profesion.component';
-import { CrearProfesionComponent } from './modulos/parametrizacion/profesion/crear-profesion/crear-profesion.component';
-import { EditarProfesionComponent } from './modulos/parametrizacion/profesion/editar-profesion/editar-profesion.component';
-import { EliminarProfesionComponent } from './modulos/parametrizacion/profesion/eliminar-profesion/eliminar-profesion.component';
+import { BarraNavegacionSuperiorComponent } from './publico/paginaMaestra/barra-navegacion-superior/barra-navegacion-superior.component';
+import { MenuLateralComponent } from './publico/paginaMaestra/menu-lateral/menu-lateral.component';
+import { PieDePaginaComponent } from './publico/paginaMaestra/pie-de-pagina/pie-de-pagina.component';
+import { InicioComponent } from './publico/inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarDepartamentoComponent,
-    CrearDepartamentoComponent,
-    EditarDepartamentoComponent,
-    EliminarDepartamentoComponent,
-    ListarCiudadComponent,
-    CrearCiudadComponent,
-    EditarCiudadComponent,
-    EliminarCiudadComponent,
-    ListarHabilidadComponent,
-    CrearHabilidadComponent,
-    EditarHabilidadComponent,
-    EliminarHabilidadComponent,
-    ListarProfesionComponent,
-    CrearProfesionComponent,
-    EditarProfesionComponent,
-    EliminarProfesionComponent
+    BarraNavegacionSuperiorComponent,
+    MenuLateralComponent,
+    PieDePaginaComponent,
+    InicioComponent,
+    
   ],
   imports: [
     BrowserModule,
